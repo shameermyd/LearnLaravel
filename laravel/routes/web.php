bfb1c53f07e720ca81e48593d9e1578387acd4a4
shameermyd <35391932+shameermyd@users.noreply.github.com>
2024-01-23 18:21:16 +0530

    update routes/web.php page
    basic get operations (passing values through url)

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello',function (){
+    return response("<h1>Hello World</h1>")
+    ->header('Content-Type','text/plain')   //in the browser console, we can check it Header
+    ;
+});
+
+Route::get('/posts/{id}',function ($id){
+    // ddd($id);                               //die dumb and debug
+    return response('Post '. $id );         //get('{id}',function ($id){ return $id })->where('id','condition');
+})->where('id','[0-9]+');
+
+Route::get('/search',function (Request $request){
+    // dd($request);
+    return response('Name: '.$request->name.' , City: '.$request->city); // url=search?name=sha&city=kollam
+});
+
