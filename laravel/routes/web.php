b38c8d6ecbeb4c6e6ecbf9af89f7cb46f9ed01a0
shameermyd <35391932+shameermyd@users.noreply.github.com>
2024-01-24 15:24:41 +0530

    create a Listing modal and data transfer
    create a Listing modal and list data transfer to view page via router.
    Router call view and Modal data

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use App\Models\Listing;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,18 +18,7 @@ use Illuminate\Http\Request;
 Route::get('/',function(){
     return view('list',[
         'heading' => 'List For Learn',
-        'listing' => [
-            [
-                'id' => '1',
-                'title'=>'List ONE',
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-            ],
-            [
-                'id' => '2',
-                'title'=>'List TWO',
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-            ]
-        ]    
+        'listing' => Listing::all()  
     ]);
 });
 
