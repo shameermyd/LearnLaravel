4f1df39315e6c811a454b3a8302c0b7dea076f45
shameermyd <35391932+shameermyd@users.noreply.github.com>
2024-01-24 15:14:30 +0530

    Update list.blade.php
    just add a conditon @unless

--- laravel/resources/views/list.blade.php
+++ laravel/resources/views/list.blade.php
@@ -1,6 +1,10 @@
 <h1>{{$heading}}</h1>
 
-@foreach($listing as $lists)
-    <h3>{{$lists['title']}}</h3>
-    <p>{{$lists['description']}}</p>
-@endforeach
+@unless(count($listing) == 0)
+    @foreach($listing as $lists)
+        <h3>{{$lists['title']}}</h3>
+        <p>{{$lists['description']}}</p>
+    @endforeach
+@else
+    <h4>No listing data is found</h4>
+@endunless
