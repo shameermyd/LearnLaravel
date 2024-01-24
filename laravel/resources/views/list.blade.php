7378569e3100131c73b5c0b528c78ce76341f042
shameermyd <35391932+shameermyd@users.noreply.github.com>
2024-01-24 15:06:00 +0530

    route data from web -> view with foreach
    multiple list data  from route page  (web.php) to (list.blade.php) view page, show all data with foreach

--- laravel/resources/views/list.blade.php
+++ laravel/resources/views/list.blade.php
@@ -1,3 +1,6 @@
 <h1>{{$heading}}</h1>
-<h3>{{$listing['title']}}</h3>
-<p>{{$listing['description']}}</p>
+
+@foreach($listing as $lists)
+    <h3>{{$lists['title']}}</h3>
+    <p>{{$lists['description']}}</p>
+@endforeach
