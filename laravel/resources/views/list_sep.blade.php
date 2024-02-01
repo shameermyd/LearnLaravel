f33eb1fa489d1dfdcbe108f30290a4d6922cb849
shameermyd <35391932+shameermyd@users.noreply.github.com>
2024-02-01 17:43:04 +0530

    create partials Like search box,banner for site
    includes search box,banner for website.
    creating a folder named  'partials' in view folder. Add 2 page in it(search,hero)

--- laravel/resources/views/list_sep.blade.php
+++ laravel/resources/views/list_sep.blade.php
@@ -1,6 +1,9 @@
 @extends('layout')
 
 @section('content')
+@include('partials._hero')
+@include('partials._search')
+
     <h3>{{$lists['title']}}</h3>
     <p>{{$lists['description']}}</p>
 @endsection
