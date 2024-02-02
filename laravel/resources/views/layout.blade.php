0a2634fb325a0e89c2e907cfbe55e96ccc15ddee
shameermyd <35391932+shameermyd@users.noreply.github.com>
2024-02-03 01:10:21 +0530

    add new logo

--- laravel/resources/views/layout.blade.php
+++ laravel/resources/views/layout.blade.php
@@ -24,13 +24,11 @@
                 },
             };
         </script>
-        <title>LaraGigs | Find Laravel Jobs & Projects</title>
+        <title>ShaZam | Learn Laravel</title>
     </head>
     <body class="mb-48">
         <nav class="flex justify-between items-center mb-4">
-            <a href="index.html"
-                ><img class="w-24" src="{{asset('images/logo.png')}}" alt="" class="logo"
-            /></a>
+            <a href="/"><img class="w-24" src="{{asset('images/logo_ShaZam_red.png')}}" alt="" class="logo"/></a>
             <ul class="flex space-x-6 mr-6 text-lg">
                 <li>
                     <a href="register.html" class="hover:text-laravel"
@@ -53,7 +51,7 @@
         <footer
             class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center"
         >
-            <p class="ml-2">Copyright &copy; 2022, All Rights reserved</p>
+            <p class="ml-2">Copyright &copy; 2024, All Rights reserved</p>
 
             <a
                 href="create.html"
